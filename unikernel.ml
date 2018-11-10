323451b26b071067ec25acc77f5362a283d8d05c
Pavel Argentov <argentoff@gmail.com>
2018-11-10 18:08:00 +0300

    No lwt.ppx

--- unikernel.ml
+++ unikernel.ml
@@ -1,3 +1,5 @@
+open Lwt.Infix
+
 module Hello (Time : Mirage_time_lwt.S) = struct
   let durations = [1; 2; 3; 4; 5]
   and iterations = 1000
@@ -8,7 +10,7 @@ module Hello (Time : Mirage_time_lwt.S) = struct
       | 0 -> Lwt.return_unit
       | c ->
          Logs.info (fun f -> f "hello from instance %i iteration %i" n (iterations - c));
-         Time.sleep_ns (Duration.of_sec n) >> loop (c - 1) n
+         Time.sleep_ns (Duration.of_sec n) >>= fun () -> loop (c - 1) n
     in
     ListLabels.map ~f:(loop iterations) durations |> Lwt.join
 end
